e388fb8b1df19520d2b0d85e5c3e4704aa7d1bca
Elnur <eeliyev@yahoo.com>
2019-10-21 19:16:57 +0400

    --initialJustStarted

---
+++ src/main/java/Homework3.java
@@ -0,0 +1,27 @@
+public class Homework3 {
+
+    public static void main(String[] args) {
+
+        String[][] schedule= new String[7][2];
+        int i,j;
+
+        schedule[0][0]="Monday";
+        schedule[0][1]="Do HomeWork for Tuesday";
+        schedule[1][0]="Tuesday";
+        schedule[1][1]="Classes for Tuesday";
+        schedule[2][0]="Wednesday";
+        schedule[2][1]="Do HomeWork for Thursday";
+        schedule[3][0]="Thursday";
+        schedule[3][1]="Classes for Thursday";
+        schedule[4][0]="Friday";
+        schedule[4][1]="Do HomeWork for Saturday";
+        schedule[5][0]="Saturday";
+        schedule[5][1]="Classes for Saturday";
+        schedule[6][0]="Sunday";
+        schedule[6][1]="Relax";
+
+
+
+
+    }
+}
